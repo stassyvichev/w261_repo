8ee319b1b0eafb26809adf3e7f49357913fc9c0f
Stanimir Vichev <stassyvichev@gmail.com>
2018-01-12 19:01:15 +0000

    new Notes and start of HW1

---
+++ HW01/collateCounts.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+This script reads word counts from STDIN and combines
+the counts for any duplicated words.
+
+INPUT & OUTPUT FORMAT:
+    word \t count
+USAGE:
+    python collateCounts.py < yourCountsFile.txt
+
+Instructions:
+    For Q6 - Use the provided code as is.
+    For Q7 - Delete the section marked "PROVIDED CODE" and replace
+             it with your own implementation. Your solution should
+             not use a dictionary or store counts in any way - just
+             print them as soon as you've added them. HINT: you've
+             modified the framework script to ensure that the input
+             is alphabetized; how can you use that to your advantage?
+"""
+
+# imports
+import sys
+from collections import defaultdict
+
+########### PROVIDED IMPLEMENTATION ##############
+counts = defaultdict(int)
+# stream over lines from Standard Input
+for line in sys.stdin:
+    # extract words & counts
+    word, count  = line.split()
+    # tally counts
+    counts[word] += int(count)
+# print counts
+for wrd, count in counts.items():
+    print("{}\t{}".format(wrd,count))
+########## (END) PROVIDED IMPLEMENTATION #########
+
+################# YOUR CODE HERE #################
+
+
+
+
+
+
+
+
+################ (END) YOUR CODE #################
--- reducer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-sum = 0
-for line in sys.stdin:
-    sum = sum + int(line)
-print(sum)
