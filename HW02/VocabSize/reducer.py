078e1f4a0f99d2ccd27b54508ce7e551576f9fc3
Stanimir Vichev <stassyvichev@gmail.com>
2018-01-20 17:44:23 +0000

    hw2 problem 4 solved

--- HW02/VocabSize/reducer.py
+++ HW02/VocabSize/reducer.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 """
-<write your description here>
+reducer to be used to count the number of unique (distinct) words in the supplied files. This accepts key,value pairs from the mapper, recording the number of unique words in the file.
 INPUT:
-    <specify record format here>
+    Key-value pairs read from stdin in the form word\tcount 
+    for a set of word and all their counts, sorted by key.
 OUTPUT:
-    <specify record format here>
+    a single count for the number of unique words. 
 """
 import re
 import sys
 
 cur_word = None
-word_count = 0
+word_count = 1
 # read from standard input
 for line in sys.stdin:
     line = line.strip()
 
 ############ YOUR CODE HERE #########
+    # tally counts from current key
+    key, value = line.split()
+    if key != cur_word: 
+        if cur_word:
+            word_count +=1
+        cur_word = key
 
-
-
-
-
-
+# don't forget the last record! 
+print word_count 
 ############ (END) YOUR CODE #########
