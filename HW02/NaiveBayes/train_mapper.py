996244caa176b16c8a08d343516ca3f694a8eb55
Stanimir Vichev <stassyvichev@gmail.com>
2018-01-23 02:12:59 +0000

    hw2 problem 9 section d) finished. To finish e) and 10. Choose implementation.

--- HW02/NaiveBayes/train_mapper.py
+++ HW02/NaiveBayes/train_mapper.py
@@ -2,9 +2,9 @@
 """
 Mapper reads in text documents and emits word counts by class.
 INPUT:
-    <specify record format here>
+    ID \t SPAM \t SUBJECT \t CONTENT \n
 OUTPUT:
-    <specify record format here>
+    word \t class \t count
     
 Instructions:
     You know what this script should do, go for it!
@@ -20,31 +20,18 @@ Instructions:
        for your conditional probabilities and class priors.
 """
 ##################### YOUR CODE HERE ####################
+import re
+import sys
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# read from standard input
+for line in sys.stdin:
+    # parse input and tokenize
+    docID, _class, subject, body = line.lower().split('\t')
+    words = re.findall(r'[a-z]+', subject + ' ' + body) 
+    print '%s\t%s\t%s' % ("*", _class, 1)
+    for word in words:
+        print '%s\t%s\t%s' % (".", _class, 1)
+        print '%s\t%s\t%s' % (word, _class, 1)
+        print '%s\t%s\t%s' % (word, -1, 1)
 
 ##################### (END) YOUR CODE #####################
--- HW02/NaiveBayes/train_reducer.py
+++ HW02/NaiveBayes/train_reducer.py
@@ -3,9 +3,10 @@
 Reducer aggregates word counts by class and emits frequencies.
 
 INPUT:
-    <specify record format here>
+    word \t class \t count
 OUTPUT:
-    <specify record format here>
+    word \t num_c0_class \t num_c1_class \t cond_prob_c0 \t cond_prob_c1
+    add ClassPriors record
     
 Instructions:
     Again, you are free to design a solution however you see 
@@ -17,8 +18,74 @@ Instructions:
     1) Don't forget to emit Class Priors (with the right key).
     2) In python2: 3/4 = 0 and 3/float(4) = 0.75
 """
-##################### YOUR CODE HERE ####################
+##################### YOUR CODE HERE #################### 
+import re
+import sys
 
+# initialize trackers
+c0_doc_count, c1_doc_count, c1_word_count, c0_word_count = 0,0,0,0
+curr_word = "*"
+curr_word_count = 0
+curr_word_c0_count = 0
+curr_word_c1_count = 0
+
+# read from standard input
+for line in sys.stdin:
+    # parse input
+    
+    word, isSpam, count = line.split('\t')
+    # print word, isSpam, count
+    isSpam = int(isSpam)
+    count = int(count)
+    if curr_word == word:
+        # we use * to denote number of documents with specific class
+        if word == "*":
+            if isSpam:
+                c1_doc_count += count
+            else:
+                c0_doc_count += count
+        elif word == ".":
+            # use . to denote count of words with specific class class
+            if isSpam:
+                c1_word_count += count
+            else:
+                c0_word_count += count
+        else:
+            # we use -1 to denote word counts
+            if isSpam == -1:
+                curr_word_count += count
+            elif isSpam == 0:
+                curr_word_c0_count += count
+            else:
+                curr_word_c1_count += count
+    else: 
+        if curr_word == "*":
+            total_count = c0_doc_count+c1_doc_count
+            print "%s\t%s\t%s\t%s\t%s" % ("ClassPriors", c0_doc_count, c1_doc_count, c0_doc_count/float(total_count), c1_doc_count/float(total_count)) 
+            if isSpam:
+                c1_word_count += count
+            else:
+                c0_word_count += count
+        elif curr_word !=".":
+            cond_prob_c0 = curr_word_c0_count/float(c0_word_count)
+            cond_prob_c1 = curr_word_c1_count/float(c1_word_count)
+            print "%s\t%s\t%s\t%s\t%s" % (curr_word, curr_word_c0_count, curr_word_c1_count,cond_prob_c0 , cond_prob_c1) 
+            curr_word_count = 0
+            curr_word_c0_count = 0
+            curr_word_c1_count = 0
+            if isSpam == -1:
+                curr_word_count += count
+            elif isSpam == 0:
+                curr_word_c0_count += count
+            else:
+                curr_word_c1_count += count
+        curr_word = word
+        
+            
+# printing the last word                
+cond_prob_c0 = curr_word_c0_count/float(c0_word_count)
+cond_prob_c1 = curr_word_c1_count/float(c1_word_count)
+print "%s\t%s\t%s\t%s\t%s" % (curr_word, curr_word_c0_count, curr_word_c1_count,cond_prob_c0 , cond_prob_c1)
 
 
 
