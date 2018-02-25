a92ed0165949a9440571b9d97c3de365e602ee2c
Stanimir Vichev <stassyvichev@berkeley-ia.service.altiscale.com>
2018-02-25 12:55:44 +0000

    HW6 problem 5 started

--- HW6/lrBatchGd.py
+++ HW6/lrBatchGd.py
@@ -1,14 +1,8 @@
+%%writefile lrBatchGd.py
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+# from numpy import random,array, dot
 import numpy as np
-
-def generate_random_weights():
-    weights = np.array([random.uniform(-3,3),random.uniform(-3,3)])
-#     print weights
-    # Write the weights to the files
-    with open('weights.txt', 'w+') as f:
-        f.writelines(','.join(str(j) for j in weights)) 
-    return weights
 
 class MrJobLRBatchGD(MRJob):
     def get_weights(self):
@@ -16,6 +10,7 @@ class MrJobLRBatchGD(MRJob):
             self.weights = [float(v) for v in f.readline().split(",")]
         self.partial_gradient = [0.0]*len(self.weights)
         self.partial_gradient = np.array(self.partial_gradient)
+        print np.array(self.partial_gradient)
         self.partial_count = 0
     
     def mapper(self, _, line):
@@ -24,14 +19,11 @@ class MrJobLRBatchGD(MRJob):
         y = float(y)
         w = np.array(self.weights)
         gdnt = np.dot(y- np.dot(x,w),x)
-#         self.partial_gradient = [self.partial_gradient[0]+ gdnt[0], self.partial_gradient[1]+ gdnt[1]]
         self.partial_gradient += gdnt
         self.partial_count +=1
-#         yield ((y- x*w)*x,1)
     
     def mapper_final(self):
         yield None, (self.partial_gradient, self.partial_count)
-    
     
     def reducer(self, _, gradients_counts):
         final_gradient = np.array([0.0]*2)
@@ -40,7 +32,6 @@ class MrJobLRBatchGD(MRJob):
             final_count +=pc
             final_gradient += pg
         yield None, [final_gradient/final_count]
-    
     
     def steps(self):
         return [
